4c6b693adfeeef356c9583fe0257ea1b0c8961ad
iishasmall <iishasmall@iishasmall.com>
2018-06-28 10:51:36 -0400

    changed git ignore file

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/iishasmall/vue-sample-website/',
+    assetsPublicPath: '/vue-sample-website/',
 
     /**
      * Source Maps
