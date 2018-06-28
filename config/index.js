f464bb03b1390a2c6296acc7139f77df8633af23
iishasmall <iishasmall@iishasmall.com>
2018-06-28 10:56:52 -0400

    new commit

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-sample-website/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
